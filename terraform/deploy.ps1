6d5aeffbb667b3f7665ab70163b7fa6c6cb9d419
pisa-kun <pisa0401mst@gmail.com>
2025-01-19 03:59:06 +0900

    fix: del comment

--- terraform/deploy.ps1
+++ terraform/deploy.ps1
@@ -7,6 +7,5 @@ param(
 aws ecr get-login-password --region $Region | docker login --username AWS --password-stdin $RepositoryUrl
 
 # Docker イメージのビルドとプッシュ
-# docker build -f ../Dockerfile -t "$RepositoryUrl:latest" .
 docker build -f .\Dockerfile -t "$RepositoryUrl:latest" .
 docker push "$RepositoryUrl:latest"
