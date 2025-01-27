a4b5c7d8297566206b477f3c99cc214314a5faa1
pisa-kun <pisa0401mst@gmail.com>
2025-01-27 23:23:41 +0900

    fix: verify path test, can ref github path

--- cloud-run-tf/terraform/api-gateway.tf
+++ cloud-run-tf/terraform/api-gateway.tf
@@ -33,10 +33,14 @@ resource "google_api_gateway_api_config" "api_gateway_config" {
 
     openapi_documents {
       document {
-        #path = "openApi.yaml"
+        # ローカルのファイルを指定
+        # path = "openApi.yaml"
         # githubのpathも指定可能
-        path = "https://github.com/pisa-kun/sample-golang-webserver/blob/main/cloud-run-tf/terraform/openApi2.yaml"
-        contents = filebase64("openApi2.yaml")
+        # 参照するyamlを切り替えること可能
+        # path = "https://github.com/pisa-kun/sample-golang-webserver/blob/main/cloud-run-tf/terraform/openApi2.yaml"
+        # contents = filebase64("openApi2.yaml")
+        path = "https://github.com/pisa-kun/sample-golang-webserver/blob/main/cloud-run-tf/terraform/openApi.yaml"
+        contents = filebase64("openApi.yaml")
       }
     }
 
