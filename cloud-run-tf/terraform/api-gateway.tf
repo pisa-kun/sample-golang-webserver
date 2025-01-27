4e74f2f286ef32e50c09c2fddca5f290c13b4511
pisa-kun <pisa0401mst@gmail.com>
2025-01-27 12:12:15 +0900

    fix: path enable set github

--- cloud-run-tf/terraform/api-gateway.tf
+++ cloud-run-tf/terraform/api-gateway.tf
@@ -33,7 +33,9 @@ resource "google_api_gateway_api_config" "api_gateway_config" {
 
     openapi_documents {
       document {
-        path = "openApi.yaml"
+        #path = "openApi.yaml"
+        # githubのpathも指定可能
+        path = "https://github.com/pisa-kun/sample-golang-webserver/blob/main/cloud-run-tf/terraform/openApi.yaml"
         contents = filebase64("openApi.yaml")
       }
     }
