8f6805ca6b980cf5f01131673eafac717ec8b8b6
pisa-kun <pisa0401mst@gmail.com>
2025-01-28 12:33:59 +0900

    fix: base64encode

--- cloud-run-tf/terraform/api-gateway.tf
+++ cloud-run-tf/terraform/api-gateway.tf
@@ -42,7 +42,7 @@ resource "google_api_gateway_api_config" "api_gateway_config" {
         path = "https://github.com/pisa-kun/sample-golang-webserver/blob/main/cloud-run-tf/terraform/openApi.yaml"
         # 動的切り替え
         # contents = filebase64("openApi.yaml")
-        contents = base64decode(templatefile("openApi.yaml",{
+        contents = base64encode(templatefile("openApi.yaml",{
           func_url = google_cloud_run_v2_service.your_service.uri
         }))
       }
