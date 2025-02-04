4ab4bb52d0e0b15ec84dbf7bf8570f25c045b5b6
pisa-kun <pisa0401mst@gmail.com>
2025-02-04 11:06:47 +0900

    fix: enable

--- tf-apigw-cloudrun-only/provider.tf
+++ tf-apigw-cloudrun-only/provider.tf
@@ -9,8 +9,21 @@ provider "google-beta" {
 }
 
 # API Gateway APIを有効化
+# gcloud services enable apigateway.googleapis.com         # API Gateway API
+# gcloud services enable servicemanagement.googleapis.com  # Service Management API
+# gcloud services enable servicecontrol.googleapis.com     # Service Control API
 # TODO: Destroy時にAPI無効化されるっぽいのでCLIで有効化必要
 # resource "google_project_service" "apigateway" {
 #   project = var.project_id
 #   service = "apigateway.googleapis.com"
+# }
+
+# resource "google_project_service" "servicemanagement" {
+#   project = var.project_id
+#   service = "servicemanagement.googleapis.com"
+# }
+
+# resource "google_project_service" "servicecontrol" {
+#   project = var.project_id
+#   service = "servicecontrol.googleapis.com"
 # }
