e7ff03b9395f0ceb117d7b0b93d83cffa6c2c5d5
pisa-kun <pisa0401mst@gmail.com>
2025-02-04 10:06:19 +0900

    fix: service account

--- tf-apigw-cloudrun-only/provider.tf
+++ tf-apigw-cloudrun-only/provider.tf
@@ -7,3 +7,9 @@ provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
+
+# API Gateway APIを有効化
+resource "google_project_service" "apigateway" {
+  project = var.project_id
+  service = "apigateway.googleapis.com"
+}
