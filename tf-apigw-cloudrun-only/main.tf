09df156bdb03383f7c92d6ec6cf366d8ef42f985
pisa-kun <pisa0401mst@gmail.com>
2025-02-13 21:17:21 +0900

    fix: path

--- tf-apigw-cloudrun-only/main.tf
+++ tf-apigw-cloudrun-only/main.tf
@@ -32,6 +32,6 @@ module "api_gateway" {
   api_display_name        = "api-gateway-dev"         # api root display name
   api_config_display_name = "api-gateway-dev-config"  # config root name
   gateway_display_name    = "api-gateway-dev-gateway" # Gateway root name
-  openapi_path            = "openApi-show.yaml"
+  openapi_path            = "openApi.copy.yaml"
   func_url                = google_cloud_run_v2_service.this.uri
 }
